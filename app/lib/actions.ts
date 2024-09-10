0a6e60bd7dcef20510b2deb7b669167c0bac924d
salmane-chougrani <salmane.chougrani@viseo.com>
2024-09-10 23:45:25 +0100

    resolve deployment

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,7 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation'; 
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
